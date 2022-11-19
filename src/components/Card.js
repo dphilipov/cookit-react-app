05f7e0319ab80ad069947e14b9a99d24b0f93204
Dimitar Filipov <grinndev@protonmail.com>
2022-11-19 16:22:31 +0200

    add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, ImagePreview } from '.';
+import { ShoppingCart } from '../icons';
+
+function Card({ recipe }) {
+  const { name, description, imageId } = recipe;
+
+  return (
+    <div className="card-container">
+      <ImagePreview imageId={imageId} quality={33} />
+      <div className="recipe-content">
+        <h2 className="recipe-name">{name}</h2>
+        <p>{description}</p>
+        <div className="buttons-container">
+          <Button theme="text" className="read-recipe-btn">
+            READ FULL RECIPE
+          </Button>
+          <Button
+            className="add-ingredients-btn"
+            icon={<ShoppingCart />}
+            theme="icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
+
+Card.propTypes = {
+  recipe: PropTypes.shape(),
+};
