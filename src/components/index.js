c384ac0050e2921e2d98fde2af4fd6fa987f5df9
Dimitar Filipov <grinndev@protonmail.com>
2022-11-20 12:19:06 +0200

    add components index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Card } from './Card';
+export { default as Button } from './Button';
+export { default as Dropzone } from './Dropzone';
+export { default as ImagePreview } from './ImagePreview';
+export { default as Input } from './Input';
+export { default as TextArea } from './TextArea';
+export { default as Modal } from './Modal';
