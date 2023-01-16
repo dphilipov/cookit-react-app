3fb3a7b06ffd5388bfb934dedb8b1c0bed6626eb
Dimitar Filipov <grinndev@protonmail.com>
2023-01-16 21:57:46 +0200

    add Trash icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,4 +2,5 @@ export { default as CookitLogo } from './CookitLogo';
 export { default as GitHubLogo } from './GitHubLogo';
 export { default as ShoppingCart } from './ShoppingCart';
 export { default as Image } from './Image';
+export { default as Trash } from './Trash';
 export { default as XIcon } from './XIcon';
