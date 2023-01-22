f44509830f7519468759a5499b3297ee2bb817aa
Dimitar Filipov <grinndev@protonmail.com>
2023-01-22 21:40:08 +0200

    add new icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,9 @@
+export { default as AddIcon } from './AddIcon';
 export { default as CookitLogo } from './CookitLogo';
 export { default as GitHubLogo } from './GitHubLogo';
 export { default as ShoppingCart } from './ShoppingCart';
 export { default as Image } from './Image';
+export { default as PlusIcon } from './PlusIcon';
+export { default as RemoveIcon } from './RemoveIcon';
 export { default as Trash } from './Trash';
 export { default as XIcon } from './XIcon';
